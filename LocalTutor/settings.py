5ec202f150feb289a6f1ef2018f6041498767a3a
MohdAinul <mohdainul01@gmail.com>
2025-01-03 12:31:16 +0530

    Temporarily switch to SQLite

--- LocalTutor/settings.py
+++ LocalTutor/settings.py
@@ -84,15 +84,10 @@ WSGI_APPLICATION = 'LocalTutor.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-       'NAME': config('DB_NAME'),
-        'USER': config('DB_USER'),
-        'PASSWORD': config('DB_PASSWORD'),
-        'HOST': config('DB_HOST'),
-        'PORT': config('DB_PORT',default='3306'),
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
