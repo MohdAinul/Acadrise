cf5b01c04bef5ade4ba0d241f1f644131c3e9d82
MohdAinul <mohdainul01@gmail.com>
2025-01-03 17:11:00 +0530

    Configured database and applied migrations

--- LocalTutor/settings.py
+++ LocalTutor/settings.py
@@ -84,10 +84,18 @@ WSGI_APPLICATION = 'LocalTutor.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': config('DB_NAME'),
+        'USER': config('DB_USER'),
+        'PASSWORD': config('DB_PASSWORD'),
+        'HOST': 'srv1667.hstgr.io',  # Use Hostinger's IPv4 address
+        'PORT': '3306',
+        'OPTIONS': {
+           'ssl': {'ssl-ca': ''},  
+        },
     }
 }
+
 
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
